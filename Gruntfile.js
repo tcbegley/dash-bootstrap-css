34caa7b732026ea8704a56a8e3c25de0a119b967
tcbegley <tom@asidatascience.com>
2019-02-28 19:27:00 +0000

    Don't generate source maps

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
     sass: {
       options: {
         implementation: require("node-sass"),
-        sourceMap: true
+        sourceMap: false
       }
     }
   });
